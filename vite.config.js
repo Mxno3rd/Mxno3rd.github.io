d70dda82914c76a093a83f1e9128f2f2e3b602be
Thanetpon Chuawong <gamelovelno@gmail.com>
2026-04-18 06:13:54 +0700

    Revert "first upload"
    This reverts commit e1a6507996515703ff4a6856914ee3fa3640ddd0.

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// [vitejs.dev](https://vitejs.dev/config/)
-export default defineConfig({
-  plugins: [react()],
-  base: '/my-portfolio/', // เปลี่ยนเป็นชื่อ repo ของคุณ
-})
